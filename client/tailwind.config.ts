a679259c5f309da9fb5d9759ff64e1de3b1971d1
ShauryaRoy <shauryaroy2004@gmail.com>
2025-06-20 10:47:29 +0530

    work around for deployment issues-3

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,11 +1,15 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                border: "#e5e7eb",
+            },
+        },
+    },
+    plugins: [],
 };
 
 export default config;
